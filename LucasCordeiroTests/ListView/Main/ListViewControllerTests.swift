ee98bcd29f99ce3ac503db98ba44be5103b5e053
Lucas Augusto Cordeiro <lucas.cordeiro@msn.com>
2019-02-16 12:23:50 -0200

    Add unit tests class

---
+++ LucasCordeiroTests/ListView/Main/ListViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ListViewControllerTests.swift
+//  LucasCordeiroTests
+//
+//  Created by Lucas Cordeiro on 16/02/19.
+//  Copyright © 2019 Sparks. All rights reserved.
+//
+
+import XCTest
+
+class ListViewControllerTests: XCTestCase {
+
+    override func setUp() {}
+
+    override func tearDown() {}
+}
---
+++ LucasCordeiroTests/ListView/Main/ListViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  ListViewModelTests.swift
+//  LucasCordeiroTests
+//
+//  Created by Lucas Cordeiro on 16/02/19.
+//  Copyright © 2019 Sparks. All rights reserved.
+//
+
+import XCTest
+
+class ListViewModelTests: XCTestCase {
+
+    override func setUp() {}
+
+    override func tearDown() {}
+}
