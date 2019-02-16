7f1ce1d19b34d6486c0ae7ff9b5bd2f466fbfd4d
Lucas Augusto Cordeiro <lucas.cordeiro@msn.com>
2019-02-15 22:28:54 -0200

    Initialize List View Empty Scene

---
+++ LucasCordeiro/List View/News Cell/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  LucasCordeiro
+//
+//  Created by Lucas Cordeiro on 15/02/19.
+//  Copyright © 2019 Sparks. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
